f26abd66ae4af752fa4409f3f46de5f3a2fd8996
fireteam99 <raymancheungsy@gmail.com>
2019-07-27 23:00:02 -0400

    added game skeleton

---
+++ plgame/src/components/App.js
@@ -0,0 +1,10 @@
+import { Client } from 'boardgame.io/react';
+import { Game } from 'boardgame.io/core';
+
+const PowerLevel = Game({
+
+});
+
+const App = Client({ game: PowerLevel });
+
+export default App;
