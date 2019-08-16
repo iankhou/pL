8eae181982c2c9c09b8931c50af617b03018d742
fireteam99 <raymancheungsy@gmail.com>
2019-08-15 21:59:03 -0400

    added card to collection component

--- plgame/src/components/Collection/Collection.js
+++ plgame/src/components/Collection/Collection.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import Card from '../Card';
+
 const Collection = props => {
-  return <div>Collection</div>;
-}
+  const { characters } = props;
+  return (
+    <div>
+      {characters.map(character => (
+        <Card
+          name={character.name}
+          description={character.description}
+          hp={character.hp}
+          attack={character.attack}
+          armor={character.armor}
+          trait={character.trait}
+          ability={character.ability}
+          ultimate={character.ultimate}
+          image={character.image}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Collection;
